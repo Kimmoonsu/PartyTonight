0d537ba617d674b68e70bb224b9fed244616a284
Kimmoonsu <visitant2@naver.com>
2020-11-30 20:53:55 +0900

    start

---
+++ src/main/java/com/christmas/partytonight/HomeController.java
@@ -0,0 +1,61 @@
+package com.christmas.partytonight;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	@Resource(name = "UserService")
+	private UserService service;
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	
+	@RequestMapping(value = "/card", method = RequestMethod.GET)
+	public String card(HttpServletRequest request, Model model) throws Exception {
+//		String name = request.getParameter("name");
+//		String str[] = service.enterTicket(name);
+//		System.out.println("str : " + str);
+//		if (str == null) {
+//			System.out.println("당신은 초대된 사람이 아닙니다.");
+//		} else {			
+//			model.addAttribute("name", str[0]);
+//			model.addAttribute("seatNumber", str[1]);
+//			System.out.println("name : " + str[0] + " seat number : " + str[1]);
+//		}
+		return "card";
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/christmas/partytonight/UserService.java
@@ -0,0 +1,62 @@
+package com.christmas.partytonight;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service("UserService")
+public class UserService {
+	private static final String PATH = "/Users/Kimmoonsu/Documents/workspace-spring-tool-suite-4-4.8.1.RELEASE/PartyTonight/src/main/resources/data/database.txt";
+	private String seatNumber[] = {"01A", "01B", "01C", "02A", "02B", "02C"};
+	public void writeFile(String name, String seat) {
+//		String path = System.getProperty("user.dir");
+//		System.out.println("path : " + path);
+		File file = new File(PATH);
+		try {
+		    BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+		    writer.write(name + " " + seat + "\n");
+		    writer.close();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+	}
+	
+	public String[] enterTicket(String name) {
+		Map<String, String> map = readFile();
+		String str[] = new String[2];
+		if (map.containsKey(name)) {
+			str[0] = name;
+			str[1] = map.get(name);
+		} else {
+			if (map.size() >= 6) {
+				return null;
+			}
+			str[0] = name;
+			str[1] = seatNumber[map.size()];
+			writeFile(name, str[1]);
+		}
+		return str;
+	}
+	
+	public Map<String, String> readFile() {
+		Map<String, String> map = new HashMap<String, String>();
+		File file = new File(PATH);
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+		    String line;
+		    while ((line = br.readLine()) != null) {
+		        String str[] = line.split(" ");
+		        map.put(str[0], str[1]);
+		    }
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		return map;
+	}
+}	
